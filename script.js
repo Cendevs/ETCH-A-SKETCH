c687a0d28d6fdb60d525f17b1f815e4b9971c5d1
Cendevs <cndigitech@gmail.com>
2020-10-02 12:33:56 +0000

    optimized

--- script.js
+++ script.js
@@ -1,17 +1,84 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
+const promptBtn = document.querySelector('#prompt');
+const colorSwitch = document.querySelector('.switch');
 
-let defGrids = (rows, cols)=>{
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for(let i = 0; i < (rows * cols); i++){
-        let squares = document.createElement('div');
-        squares.innerHTML = (i+1);
-        squares.className = 'grid-item';
-        container.appendChild(squares) 
-    }
+
+
+function selectColor(){
+    let randomColor = '#' +Math.floor(Math.random() * 1677715).toString(16);
+    return randomColor;
 }
+
+
+let defGrids = (value)=>{
+    let values = value * value;
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    for(let i = 0; i < values; i++){
+        const square = document.createElement('div');
+        container.appendChild(square)
+        square.classList.add('grid-item');
+
+}
+    const squares = document.querySelectorAll('.grid-item');
+    squares.forEach(square =>{
+    square.addEventListener('mouseover', ()=>{
+    square.style.backgroundColor = 'var(--pen-color)'
+
+})
+})
+}
+
+function removeGrid(){
+    container.innerHTML = "";
+    const squares = document.querySelectorAll('.grid-item');
+    squares.forEach(square =>{
+    square.classList.remove('.grid-item')
+   });
+}
+
+promptBtn.addEventListener('click', ()=>{
+    newGrid();
     
-defGrids(6, 6)
+});
+
+function newGrid(){
+    removeGrid();
+    value = prompt('How many square do you want to sketch on each side?');
+    defGrids(value);
+}
+
+promptBtn.textContent ="Create New Grid";
+colorSwitch.textContent = "Random Color";
+
+function leavingApixelatedTrail(){
+    colorSwitch.onclick = () => {
+        const squares = document.querySelectorAll('.grid-item');
+        squares.forEach(square =>{
+        square.addEventListener('mouseover', ()=>{
+        square.style.backgroundColor = selectColor();
+    });
+    })
+    } 
+}
+
+defGrids(16);
+leavingApixelatedTrail();
+
+
+
+
+
+
+
+
+ 
+
+
+    
+
+
+
 
 
 
